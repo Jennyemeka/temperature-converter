f9a66ed379ca12467dd7c5858906160f84d6d53b
jenni_emeka <jenniferemeka24@gmail.com>
2024-01-13 18:54:55 +0100

    updated

--- src/temperatureConverter/TemperatureConverter.java
+++ src/temperatureConverter/TemperatureConverter.java
@@ -4,7 +4,7 @@ import javax.swing.JOptionPane;
 public class TemperatureConverter {
 
     public static void main(String[] args) {
- String temperatureSelect = JOptionPane.showInputDialog("Select a converion:\n1.Fahrenheit to Celsius \n2. Celsius to Fahrenheit");
+ String temperatureSelect = JOptionPane.showInputDialog("Select a converion:\n1. Fahrenheit to Celsius \n2. Celsius to Fahrenheit");
  if(temperatureSelect.equals("1")) {
 	String UserInput1 = JOptionPane.showInputDialog("Enter your Fahrenheit value");
 	double valueF = Double.parseDouble(UserInput1);
@@ -12,7 +12,7 @@ public class TemperatureConverter {
 	JOptionPane.showMessageDialog(null, "your whether is: " + result1 + "°F");
 
 }else if(temperatureSelect.equals("2")) {
-	String UserInput2 = JOptionPane.showInputDialog("Enter a celsius vaue");
+	String UserInput2 = JOptionPane.showInputDialog("Enter a celsius value");
 	double valueC = Double.parseDouble(UserInput2);
 	double result2 = (valueC + 32)* 5/9;
 	JOptionPane.showMessageDialog(null, "your whether is: " + result2 + "°C");
